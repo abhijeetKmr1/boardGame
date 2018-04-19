4f5dde44a89a73a999a96e9fba68f7e3214cd382
cyadav1010 <cyadav1010@gmail.com>
2018-04-19 09:53:58 -0700

    Add files via upload

---
+++ BoardGame/src/main/coin.java
@@ -0,0 +1,19 @@
+package Coin;
+
+public class coin {
+ Integer x;
+ Integer y;
+ Integer radius=2;
+
+ coin(Integer x,Integer y){
+     this.x=x;
+     this.y=y;
+ }
+
+    public Integer getX() {
+        return x;
+    }
+    public Integer getY(){
+     return y;
+    }
+}
